80642ff0fb13abc9e824f9faf9e4031384be468a
SjsmithUIC <sjsmith7@uic.edu>
2017-11-30 01:12:33 -0600

    Terrain extended. Soldiers can be deleted
    TODO
    Delete soldiers at right time
    Rewpawn soldiers

---
+++ FlandersFields/Assets/Scripts/SoldieerRespawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldieerRespawn : MonoBehaviour 
+{
+	public pref
+	public GameObject[] soldiers;
+	public Transform[] Spawpoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i = 0; i < soldiers.Length; i++) 
+		{
+			if (soldiers [i] != null) 
+			{
+				if (soldiers [i].transform.position.x < 400) 
+				{
+					Destroy (soldiers [i]);
+				}
+			}
+		}
+
+		if (soldiers.Length == 0) 
+		{
+			int SpawnLocation = Random.Range (0, Spawpoints.Length);
+
+
+		}
+	}
+}
